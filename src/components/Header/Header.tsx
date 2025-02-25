5f77e9376b68bf6b444d91079900e4aa74b7f8ea
Михаил Нечаев <theh4ck3r@yandex.ru>
2025-02-25 15:12:52 +0300

    add: Modal in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,46 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./Header.module.scss";
 import { LogoIcon } from "@/public/index";
-import Avatar from "@mui/material/Avatar";
+import { Box, Button, Modal, Typography } from "@mui/material";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className={styles.root}>
       <div className={styles.wrapper}>
         <Link href={"/"}>
           <LogoIcon></LogoIcon>
         </Link>
-        <Avatar sx={{ bgcolor: "var(--gs-color-primary)" }}></Avatar>
+        <Button color="error" onClick={() => setIsModalOpen(true)}>
+          Выйти
+        </Button>
+
+        <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          <Box className={styles.modal}>
+            <Typography variant="h6" component="h2">
+              Вы точно хотите выйти?
+            </Typography>
+
+            <div className={styles.modal_buttons}>
+              <Button variant="contained" onClick={() => setIsModalOpen(false)}>
+                Нет
+              </Button>
+
+              {/* TODO: Сделать логику выхода из аккаунта */}
+              <Button
+                color="error"
+                variant="contained"
+                onClick={() => setIsModalOpen(false)}
+              >
+                Да
+              </Button>
+            </div>
+          </Box>
+        </Modal>
       </div>
     </div>
   );
