9d4b11605943e9cb0e07aa78f960d5995b42d634
Михаил Нечаев <theh4ck3r@yandex.ru>
2025-02-25 14:46:18 +0300

    update: Data

--- src/components/Data/Data.tsx
+++ src/components/Data/Data.tsx
@@ -4,6 +4,13 @@ import { kNasaApiKey } from "@/common/app";
 import styles from "./Data.module.scss";
 import useSWR from "swr";
 import { GETBaseFetcher } from "@/common/fetcher";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Skeleton,
+} from "@mui/material";
 
 export const Data = () => {
   const {
@@ -14,7 +21,41 @@ export const Data = () => {
     return `planetary/apod?api_key=${kNasaApiKey}`;
   }, GETBaseFetcher);
 
-  console.log(data, dataLoading, dataError);
+  if (dataLoading) {
+    return (
+      <div className={styles.root}>
+        <div className={styles.wrapper}>
+          {/* TODO: Сделать нормально скелетоны */}
+          {[1, 2, 3, 4, 5, 6].map((i) => (
+            <Skeleton
+              key={i}
+              variant="rectangular"
+              width={401}
+              height={401}
+              animation="wave"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 
-  return <div className={styles.root}>{data?.date}</div>;
+  if (dataError) {
+    return <div className={styles.error}>{"Ошибка :("}</div>;
+  }
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.wrapper}>
+        {Array(data)?.map((data, index: number) => (
+          <Card key={index}>
+            <CardContent>{data.date}</CardContent>
+            <CardActions>
+              <Button size="small">Подробнее</Button>
+            </CardActions>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 };
