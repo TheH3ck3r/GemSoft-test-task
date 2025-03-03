1e1288d6f3d9a8be8dbd8e517b6b239fe4159230
Михаил Нечаев <theh4ck3r@yandex.ru>
2025-03-03 10:27:39 +0300

    update: DataInfo

--- src/components/DataInfo/DataInfo.tsx
+++ src/components/DataInfo/DataInfo.tsx
@@ -6,7 +6,7 @@ import styles from "./DataInfo.module.scss";
 import { VacancyFetcher } from "@/common/fetcher";
 import Link from "next/link";
 import { BackArrowIcon } from "@/public/index";
-import { Skeleton } from "@mui/material";
+import { Divider, Skeleton } from "@mui/material";
 import Image from "next/image";
 import { departmentsImages } from "@/common/images";
 
@@ -26,6 +26,7 @@ export const DataInfo = () => {
     return `${uuid}`;
   }, VacancyFetcher);
 
+  // TODO: Исправить ошибку с неработающей ошибкой и скелетонами
   if (dataInfoLoading) {
     return (
       <div className={styles.root}>
@@ -67,20 +68,76 @@ export const DataInfo = () => {
 
       <div className={styles.wrapper}>
         <Image
-          width={500}
-          height={100}
+          width={1000}
+          height={300}
           className={styles.photo_image}
           src={departmentsImages(dataInfo?.department)?.photoUrl}
           alt="image"
         ></Image>
-        {/* <div className={styles.logo}>
-          <Image
-            width={96}
-            height={96}
-            src={departmentsImages(dataInfo?.department)?.logoUrl}
-            alt="image"
-          ></Image>
-        </div> */}
+
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <div className={styles.logo}>
+              <Image
+                width={96}
+                height={96}
+                src={departmentsImages(dataInfo?.department)?.logoUrl}
+                className={styles.logo_image}
+                alt="image"
+              ></Image>
+            </div>
+
+            <div className={styles.title_text}>{dataInfo?.name}</div>
+
+            <div className={styles.title_categories}>
+              <div className={styles.title_info}>
+                <div className={styles.title_info_head}>Локация</div>
+                <div className={styles.title_info_bottom}>
+                  {dataInfo?.location}
+                </div>
+              </div>
+
+              <div className={styles.title_info}>
+                <div className={styles.title_info_head}>Уровень</div>
+                <div className={styles.title_info_bottom}>
+                  {dataInfo?.level}
+                </div>
+              </div>
+
+              <div className={styles.title_info}>
+                <div className={styles.title_info_head}>Отдел</div>
+                <div className={styles.title_info_bottom}>
+                  {dataInfo?.department}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Divider></Divider>
+
+          <div style={{ marginTop: "40px" }}>
+            <div className={styles.description}>Описание</div>
+            <p className={styles.description_info}>{dataInfo?.info}</p>
+          </div>
+
+          <div style={{ marginTop: "40px" }}>
+            <div className={styles.description}>Требования</div>
+            <ul className={styles.description_list}>
+              {dataInfo?.requirements.map((item: string, index: number) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div style={{ marginTop: "40px" }}>
+            <div className={styles.description}>Задачи</div>
+            <ul className={styles.description_list}>
+              {dataInfo?.tasks.map((item: string, index: number) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
