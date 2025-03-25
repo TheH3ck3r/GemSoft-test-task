c29595b9c3e3b7ced461ce5e9b8daa9e20173621
Михаил Нечаев <theh4ck3r@yandex.ru>
2025-03-25 10:17:26 +0300

    add: vacanciesPageSettingsStore

---
+++ common/stores/vacanciesPageSettingsStore.ts
@@ -0,0 +1,32 @@
+import { VacanciesPageSettings } from "@/data-types/props";
+import { makeAutoObservable } from "mobx";
+
+export class vacanciesPageSettingsStore {
+  vacanciesPageSettings: VacanciesPageSettings = {
+    isSearch: true,
+    isCards: true,
+  };
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setVacanciesPageSettings(data: VacanciesPageSettings) {
+    this.vacanciesPageSettings = data;
+  }
+
+  updateVacanciesPageSettings(partialData: Partial<VacanciesPageSettings>) {
+    this.vacanciesPageSettings = {
+      ...this.vacanciesPageSettings,
+      ...partialData,
+    };
+  }
+
+  get isSearch(): boolean {
+    return this.vacanciesPageSettings.isSearch;
+  }
+
+  get isCards(): boolean {
+    return this.vacanciesPageSettings.isCards;
+  }
+}
