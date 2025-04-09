ee9d82911bb4470950ab9dedef113067eee463c0
Михаил Нечаев <theh4ck3r@yandex.ru>
2025-04-09 17:11:44 +0300

    update: UserInfo

--- src/app/users/[id]/_components/UserInfo.tsx
+++ src/app/users/[id]/_components/UserInfo.tsx
@@ -4,22 +4,22 @@ import { useEffect, useState } from "react";
 import styles from "./UserInfo.module.scss";
 import { Back } from "@/components/Back";
 import { getUserById } from "@/common/fetcher";
+import { UserProps } from "@/types/props";
 
 export const UserInfo = () => {
-  const [user, setUser] = useState([]);
-
-  console.log(user);
+  const [user, setUser] = useState<UserProps>();
 
   useEffect(() => {
     const { pathname } = window.location;
-    console.log(pathname);
-    getUserById("0").then(setUser);
+    getUserById(pathname.split("/")[2]).then(setUser);
   }, []);
   return (
     <div className={styles.root}>
       <Back text="Все пользователи" path="/users" />
 
-      <div>Тут будет инфа о пользователе</div>
+      <div>
+        {user?.firstName} {user?.lastName} {user?.middleName}
+      </div>
     </div>
   );
 };
