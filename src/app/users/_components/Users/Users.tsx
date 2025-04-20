767fd2958f374655657cb84fe677dfd5c1c7c313
Михаил Нечаев <theh4ck3r@yandex.ru>
2025-04-20 18:20:11 +0300

    fix: UserData

--- src/app/users/_components/Users/Users.tsx
+++ src/app/users/_components/Users/Users.tsx
@@ -4,19 +4,35 @@ import Link from "next/link";
 import styles from "./Users.module.scss";
 import { Button } from "@mui/material";
 import { UsersData } from "../UsersData";
-import { deleteUsers } from "@/common/fetcher";
+import { deleteUsers, getAllUsers } from "@/common/fetcher";
+import { useEffect, useState } from "react";
 
-export const Users = () => (
-  <div className={styles.root}>
-    <div className={styles.buttons}>
-      <Link href={"/users/create"}>
-        <Button>Добавить пользователя</Button>
-      </Link>
-      <Button color="error" onClick={() => deleteUsers()}>
-        Очистить
-      </Button>
+export const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getAllUsers().then(setUsers);
+  }, []);
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.buttons}>
+        <Link href={"/users/create"}>
+          <Button>Добавить пользователя</Button>
+        </Link>
+        <Button
+          color="error"
+          onClick={async () => {
+            await deleteUsers();
+            const updatedUsers = await getAllUsers();
+            setUsers(updatedUsers);
+          }}
+        >
+          Очистить
+        </Button>
+      </div>
+
+      <UsersData users={users} />
     </div>
-
-    <UsersData />
-  </div>
-);
+  );
+};
--- src/app/users/_components/UsersData/UsersData.tsx
+++ src/app/users/_components/UsersData/UsersData.tsx
@@ -3,28 +3,28 @@
 import Link from "next/link";
 import styles from "./UsersData.module.scss";
 import { UserProps } from "@/data-types/props";
-import { useEffect, useState } from "react";
-import { getAllUsers } from "@/common/fetcher";
+import { FC } from "react";
 import { observer } from "mobx-react-lite";
 
-// TODO: Исправить ошибку, когда при очистке всех данных нужно обновлять страницу, чтобы увидеть результат
-export const UsersData = observer(() => {
-  const [users, setUsers] = useState([]);
+type UsersDataProps = {
+  users: UserProps[];
+};
 
-  useEffect(() => {
-    getAllUsers().then(setUsers);
-  }, []);
-
-  return (
-    <div className={styles.root}>
-      {users.map((user: UserProps, index) => (
-        <Link key={index} href={`/users/${index}`}>
-          <div className={styles.user_card}>
-            <div>{user.firstName}</div>
-            <div>{user.lastName}</div>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
-});
+export const UsersData: FC<UsersDataProps> = observer(({ users }) => (
+  <>
+    {users.length > 0 ? (
+      <div className={styles.root}>
+        {users.map((user: UserProps, index) => (
+          <Link key={index} href={`/users/${index}`}>
+            <div className={styles.user_card}>
+              <div>{user.firstName}</div>
+              <div>{user.lastName}</div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    ) : (
+      <div className={styles.no_data}>Данных нет</div>
+    )}
+  </>
+));
